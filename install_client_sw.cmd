7c4d53ac5b367158e28fae5731194a1a7638bb8b
josef lahmer <josy1024@gmail.com>
2015-09-29 16:00:37 +0200

    Create install_client_sw.cmd
    client setup script

---
+++ install_client_sw.cmd
@@ -0,0 +1,25 @@
+:: chocolatey powershell scripts setup
+:: iex ((new-object net.webclient).DownloadString('https://chocolatey.org/%command%.ps1'))
+
+command=update
+command=install
+
+choco %command% googlechrome
+choco %command% flashplayerplugin
+choco %command% flashplayeractivex
+choco %command% silverlight
+choco %command% javaruntime
+choco %command% notepadplusplus.%command%
+choco %command% 7zip.%command%
+choco %command% vlc
+#choco %command% svn
+choco %command% tortoisesvn
+choco %command% paint.net
+:: choco %command% sublimetext:: 
+choco %command% visualstudiocode
+choco %command% jre8
+choco %command% gimp
+choco %command% cdburnerxp
+choco %command% sumatrapdf
+choco %command% googledrive
+choco %command% git
