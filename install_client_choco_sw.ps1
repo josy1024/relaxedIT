3cc665751ae93a007e22c73918abbff340624eec
josef lahmer <josy1024@gmail.com>
2022-11-11 08:45:15 +0100

    cmd to ps1

---
+++ install_client_choco_sw.ps1
@@ -0,0 +1,26 @@
+#:: 1) setup chocolatey with powershell (ADMIN SHELL!!)
+
+PS> Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+#:: 2) run this script to install or update
+
+#:: set command=update
+#:: update with: "cup all -y"
+
+
+choco upgrade googlechrome -y
+choco upgrade 7zip.%command% -y
+choco upgrade vlc -y
+choco upgrade paint.net -y
+choco upgrade visualstudiocode -y
+choco upgrade jre8 -y
+choco upgrade gimp -y
+choco upgrade sumatrapdf -y
+choco upgrade googledrive -y
+choco upgrade git -y
+choco upgrade etcher -y
+choco install libreoffice-fresh -y
+
+# :: choco upgrade cdburnerxp -y
+# choco upgrade quicktime -y
+# quicktime für goprostudi
